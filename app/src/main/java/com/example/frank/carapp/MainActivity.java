1ee0af878b5b9647ef85f9403c6fc15293053345
frank <frankntr@gmail.com>
2015-05-18 08:15:30 -0500

    first car app

--- app/src/main/java/com/example/frank/carapp/MainActivity.java
+++ app/src/main/java/com/example/frank/carapp/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        int a=0;
         return true;
+
     }
 
     @Override
